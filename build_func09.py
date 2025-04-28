bc8a01c2414333347ddaf572bfcbd95e671354db
M A C W I N <mavzurovnurali@gmail.com>
2025-04-29 00:29:55 +0500

    sss

--- build_func09.py
+++ build_func09.py
@@ -9,4 +9,8 @@ def main(x, y):
     Returns:
         int: the value of the expression
     """
-    return
+    return 2*(y**3 + x**2*y)
+
+y=int(input("sonni kiriting:"))
+x=int(input("sonni kiriting:"))
+print(main(x, y))
